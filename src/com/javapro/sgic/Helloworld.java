f02637ccd61988f04815458b3418e59e5477b28d
Thilakshana123 <nayanthara39@gmail.com>
2021-03-19 15:00:23 +0530

    change

--- src/com/javapro/sgic/Helloworld.java
+++ src/com/javapro/sgic/Helloworld.java
@@ -3,33 +3,20 @@ package com.javapro.sgic;
 public class Helloworld {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
-
-
-
-
-		
 		System.out.println("Roshany Subramaniyam");
 		System.out.println("COMPUTER");
 		System.out.println("Hello world");
 		System.out.println("CODE");
 		System.out.println("Hello I am Umasuthan");
-
-		System.out.println("sasikala");
-		
-		System.out.println("Hello I am Umasuthan");
 		System.out.println("JAFFNA");
+		System.out.println("sasikala");
+		System.out.println("JAFFNA");
+		System.out.println("Hello I am Umasuthan");
+		System.out.println("WElcome");
 		System.out.println("SGIC");
 		System.out.println("Hello");
 		System.out.println("Hello Umasuthan, Im Prgs");
 		System.out.println("organization");
-<<<<<<< HEAD
-=======
-
-
-
-
-
->>>>>>> 9184f0a323649e89a5abd71ea735c06b55cef8cf
 		System.out.println("organization");
 		System.out.println("hi");
 		System.out.println("Mathematics");
