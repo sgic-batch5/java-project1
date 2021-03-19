92eb4a2d6d9a33c197d4e9383652fcbfb393fc4e
Risha Ahamadh <rishaahamadh2@gmail.com>
2021-03-19 15:07:11 +0530

    now ok

--- src/com/javapro/sgic/Helloworld.java
+++ src/com/javapro/sgic/Helloworld.java
@@ -3,13 +3,8 @@ package com.javapro.sgic;
 public class Helloworld {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
-<<<<<<< HEAD
 		System.out.println("Risha Ahamadh...");
 		
-=======
 		System.out.println("kiruththigan");
->>>>>>> 83d0b12991d0d1be1ee657a11a75c4241233ac15
-
-		System.out.println("Theebihai");
 	}
 }
